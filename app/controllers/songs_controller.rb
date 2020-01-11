3ec1203052b1ab7e5885c240990c6ff634e337dd
CharityParks <tuppercharity@gmail.com>
2020-01-10 19:45:21 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,10 @@
 class SongsController < ApplicationController
+    require 'sinatra/base'
+    #require 'rack-flash'
+
+    get '/songs' do
+        @songs = Song.all 
+        erb :'songs/index'
+    end
+        
 end
--- app/models/song_genres.rb
+++
@@ -1,4 +0,0 @@
-class Song_genre < ActveRecord::Base
-    belongs_to :song 
-    belongs_to :genre
-end
